d165d2f43e9832a2d106b143cf4f1c46b2c1e24c
mnordkvi <martin.nordkvist@sogeti.se>
2017-07-03 16:49:22 +0200

    Logic implemented

---
+++ Fotbollstips/Logic/DataLogic.cs
@@ -0,0 +1,157 @@
+﻿using Fotbollstips.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fotbollstips.Logic
+{
+    public class DataLogic
+    {
+        public static string RATT_SVAR = "Rätt svar";
+
+        public static List<TipsDataDisplay> GetDataForPresentation()
+        {
+            try
+            {
+                // Get raw data
+                List<TipsData> rawData = GetEmTipsRawData();
+
+                // Calculate points
+                List<TipsData> calculatedPoints = CalculatePoints(rawData);
+
+                // Sort list by points
+                List<TipsData> sortedList = calculatedPoints.OrderByDescending(o => o.Poäng).ToList();
+
+                // Manipulate text string 
+                List<TipsData> manipulatedTextList = ManipulateTextStrings(sortedList);
+
+                List<TipsDataDisplay> returnList = new List<TipsDataDisplay>();
+
+                foreach (var item in manipulatedTextList)
+                {
+                    returnList.Add(new TipsDataDisplay(item));
+                }
+
+                return returnList;
+            }
+            catch (Exception e)
+            {
+                using (var db = new MartinDatabaseEntities())
+                {
+                    TipsError error = new TipsError()
+                    {
+                        Exception = e.ToString(),
+                        InnerException = e.InnerException != null ? e.InnerException.ToString() : "NULL",
+                        EntryDate = DateTime.UtcNow
+                    };
+
+                    db.TipsErrors.Add(error);
+                    db.SaveChanges();
+                }
+                return null;
+            }
+        }
+
+        private static List<TipsData> GetEmTipsRawData()
+        {
+            using (var db = new MartinDatabaseEntities())
+            {
+                var result = (from hits in db.TipsDatas
+                              select hits).ToList();
+
+                return result;
+            }
+        }
+
+        private static TipsData GetRattSvarEntity()
+        {
+            var result = GetEmTipsRawData();
+
+            var output = (from hits in result
+                          where hits.Namn == RATT_SVAR
+                          select hits).FirstOrDefault();
+
+            return output;
+        }
+
+
+        private static List<TipsData> CalculatePoints(List<TipsData> data)
+        {
+            var correct = (from hits in data
+                           where hits.Namn == RATT_SVAR
+                           select hits).FirstOrDefault();
+
+            foreach (var item in data)
+            {
+                if (item.Namn == RATT_SVAR)
+                {
+                    //TODO Ändra till 51
+                    item.Poäng = 7;
+                    continue;
+                }
+                int points = 0;
+
+                #region compare games
+
+                if (item.Sverige_Kamerun == correct.Sverige_Kamerun)
+                    points++;
+
+                if (item.Ryssland_Brasilien == correct.Ryssland_Brasilien)
+                    points++;
+
+                if (item.Kamerun_Brasilien == correct.Kamerun_Brasilien)
+                    points++;
+
+                if (item.Sverige_Ryssland == correct.Sverige_Ryssland)
+                    points++;
+
+                if (CompareFinal(item.Finallag1, correct.Finallag1))
+                    points++;
+
+                if (CompareFinal(item.Finallag2, correct.Finallag2))
+                    points++;
+
+                if (CompareFinal(item.Vinnare, correct.Vinnare))
+                    points++;
+
+
+
+                #endregion
+
+                item.Poäng = points;
+            }
+
+            return data;
+        }
+
+        private static bool CompareFinal(string finalTeam, string correctAnswer)
+        {
+            var correctTeams = correctAnswer.Split('-').ToList();
+
+            foreach (var item in correctTeams.ToList())
+            {
+                if (item.ToString() == finalTeam)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        private static List<TipsData> ManipulateTextStrings(List<TipsData> data)
+        {
+            foreach (var item in data)
+            {
+                item.Sverige_Kamerun = item.Sverige_Kamerun.Insert(1, "-");
+                item.Ryssland_Brasilien = item.Ryssland_Brasilien.Insert(1, "-");
+                item.Kamerun_Brasilien = item.Kamerun_Brasilien.Insert(1, "-");
+                item.Sverige_Ryssland = item.Sverige_Ryssland.Insert(1, "-");
+            }
+
+            return data;
+        }
+    }
+}
---
+++ Fotbollstips/Models/TipsDataDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fotbollstips.Models
+{
+    public class TipsDataDisplay
+    {
+        public string Namn { get; set; }
+        public int? Poäng { get; set; }
+        public string Finallag1 { get; set; }
+        public string Finallag2 { get; set; }
+        public string Vinnare { get; set; }
+        public string Sverige_Kamerun { get; set; }
+        public string Ryssland_Brasilien { get; set; }
+        public string Kamerun_Brasilien { get; set; }
+        public string Sverige_Ryssland { get; set; }
+
+        public TipsDataDisplay(TipsData td)
+        {
+            Namn = td.Namn;
+            Poäng = td.Poäng;
+            Finallag1 = td.Finallag1;
+            Finallag2 = td.Finallag2;
+            Vinnare = td.Vinnare;
+            Sverige_Kamerun = td.Sverige_Kamerun;
+            Ryssland_Brasilien = td.Ryssland_Brasilien;
+            Kamerun_Brasilien = td.Kamerun_Brasilien;
+            Sverige_Ryssland = td.Sverige_Kamerun;
+        }
+    }
+}
